f00f78d549365c6b129cbeab0f80443015be3f6f
Administrator <Administrator@SC-201704032046>
2018-07-15 15:49:25 +0800

    生命周期测试

---
+++ src/test/java/com/ww/test/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package com.ww.test;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.ww.config.ConfigOfLifeCycle;
+
+public class BeanLifeCycleTest {
+
+	/**
+	 * 测试bean的生命周期方法
+	 */
+	@Test
+	public void test01() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				ConfigOfLifeCycle.class);
+
+		System.out.println("容器创建完成...");
+		applicationContext.getBean("car");
+		
+		//查看容器中所有的bean，getBeanDefinitionNames
+		String[] definitionNames = applicationContext.getBeanDefinitionNames();
+		for (String name : definitionNames) {
+			System.out.println(name);
+		}
+		
+		// 关闭容器
+		applicationContext.close();
+	}
+
+}
